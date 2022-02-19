acb2ab025018fb65b1a5ceae18959f4746d9aeb2
baesunghu <chrisbae0204@gmail.com>
2022-02-19 23:22:22 +0900

    baesunghu chrisbae0204@gmail.com

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-import os
-
-os.system("pip install -r install.txt")
+import win32print
+import win32api
+import lib
+a=lib.lib(1)
+a.PrintPrinters(2)
+win32api.ShellExecute(0, 'print', "test.pdf",
+                      f'/d:"{"Samsung M332x 382x 402x Series,Samsung M332x 382x 402x Series Class Driver"}"', '.', 0)
